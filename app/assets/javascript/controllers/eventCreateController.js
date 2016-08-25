aa6a1d12d05360060c23daea6cc11b008fe1f8cc
Rob Cabardo <rob.cabardo@daugherty.com>
2016-08-25 17:05:57 -0500

    Add feature to add new events

--- app/assets/javascript/controllers/eventCreateController.js
+++ app/assets/javascript/controllers/eventCreateController.js
@@ -1,3 +1,13 @@
-angular.module('EventSquared').controller('eventCreateController', function() {
+angular.module('EventSquared').controller('eventCreateController', function($scope, Event, $location) {
+  $scope.event = new Event();
+  $scope.isSubmitting = false;
 
+  $scope.saveEvent = function(event) {
+    $scope.isSubmitting = true;
+    event.$save().then(function() {
+      $location.path('/events');
+    }).finally(function() {
+      $scope.isSubmitting = false;
+    });
+  }
 });
